fae29e750c69dd7b7f85e9b41abce58613f9877d
fvckcherrys <gianluca.auriemma4@gmail.com>
2025-11-12 19:43:55 +0100

    final commit (hopefully)

---
+++ iNote/Views/DrawingCanvasView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingCanvasView.swift
+//  iNote
+//
+//  Created by Gianluca Auriemma on 12/11/25.
+//
+
+import SwiftUI
+
+struct DrawingCanvasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingCanvasView()
+}
---
+++ iNote/Views/TextEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  TextEditorView.swift
+//  iNote
+//
+//  Created by Gianluca Auriemma on 12/11/25.
+//
+
+import SwiftUI
+
+struct TextEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextEditorView()
+}
